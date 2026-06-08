23de42c9096ced9f9967296876182d3afe1a20c8
Dennis Ward <144068419+Maiar0@users.noreply.github.com>
2026-06-08 21:44:53 +0000

    path logic

--- backend/internal/chess/move_logic.go
+++ backend/internal/chess/move_logic.go
@@ -4,9 +4,20 @@ import (
 	"fmt"
 )
 
+type Direction struct {
+	DX int
+	DY int
+}
+
 func CheckMove(piece rune, from Position, to Position) (bool, rune, error) {
 	var dy = from.col - to.col
 	var dx = from.row - to.row
+	if dx == 0 && dy == 0 {
+		return false, '0', fmt.Errorf("to position matchs from position: %v, %v", to, from)
+	}
+	if inBounds(to) {
+		return false, '0', fmt.Errorf("to position out of bound: %v", to)
+	}
 	switch piece {
 	case 'p':
 		dy *= -1
@@ -15,31 +26,41 @@ func CheckMove(piece rune, from Position, to Position) (bool, rune, error) {
 			return true, 'm', nil
 		case dy == 0 && dx == 2 && from.row == 1:
 			return true, 'd', nil
-		case abs(dy) == 1 && dx == 1:
-			return false, '0', fmt.Errorf("this is a capture: %s", from)
 		}
 	case 'P':
-		//stuff
-	case 'r':
-		//stuff
-	case 'R':
-		//stuff
-	case 'n':
-		//stuff
-	case 'N':
-		//stuff
-	case 'b':
-		//stuff
-	case 'B':
-		//stuff
-	case 'q':
-		//stuff
-	case 'Q':
-		//stuff
-	case 'k':
-		//stuff
-	case 'K':
-		//stuff
+		switch {
+		case dy == 0 && dx == 1:
+			return true, 'm', nil
+		case dy == 0 && dx == 2 && from.row == 1:
+			return true, 'd', nil
+		case abs(dy) == 1 && dx == 1:
+			return false, '0', fmt.Errorf("this is a capture: %v", from)
+		}
+	case 'r', 'R':
+		if (dx == 0 && dy != 0) || (dy == 0 && dx != 0) {
+			return true, 'm', nil
+		}
+	case 'n', 'N':
+		for _, dir := range knightDirections {
+			if dx == dir.DX && dy == dir.DY {
+				return true, 'm', nil
+			}
+		}
+	case 'b', 'B':
+		if abs(dx) == abs(dy) {
+			return true, 'm', nil
+		}
+	case 'q', 'Q':
+		if abs(dx) == abs(dy) {
+			return true, 'm', nil
+		}
+		if (dx == 0 && dy != 0) || (dy == 0 && dx != 0) {
+			return true, 'm', nil
+		}
+	case 'k', 'K':
+		if abs(dx) <= 1 && abs(dy) <= 1 {
+			return true, 'm', nil
+		}
 	default:
 		return false, '0', fmt.Errorf("invalid char: %s", piece)
 
@@ -52,4 +73,47 @@ func abs(x int) int {
 		return -x
 	}
 	return x
+}
+
+func sign(n int) int {
+	if n > 0 {
+		return 1
+	}
+	if n < 0 {
+		return -1
+	}
+	return 0
+}
+
+var knightDirections = []Direction{
+	{DX: 2, DY: 1},
+	{DX: 2, DY: -1},
+	{DX: -2, DY: 1},
+	{DX: -2, DY: -1},
+	{DX: 1, DY: 2},
+	{DX: 1, DY: -2},
+	{DX: -1, DY: 2},
+	{DX: -1, DY: -2},
+}
+
+func inBounds(pos Position) bool {
+	if pos.row > 7 || pos.row < 0 || pos.col > 7 || pos.col < 0 {
+		return false
+	}
+	return true
+}
+
+func checkPath(b Board, from Position, to Position) bool {
+	dx := sign(from.row - to.row)
+	dy := sign(from.col - to.col)
+	cur := from
+	for i := 0; i < 7; i++ {
+		cur.row += dx
+		cur.col += dy
+		p, _ := GetPiece(b, cur)
+		if p != '.' && cur != to {
+			return false
+		}
+	}
+	return true
 }
